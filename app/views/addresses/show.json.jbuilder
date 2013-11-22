fa61d512112c2b861187cde2c8c14fa4d72e0423
Ken Lu <ken@seeice.com>
2013-11-22 08:12:06 -0500

    added address model

---
+++ app/views/addresses/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @address, :street, :zip_code, :created_at, :updated_at
---
+++ db/migrate/20131122131139_create_addresses.rb
@@ -0,0 +1,10 @@
+class CreateAddresses < ActiveRecord::Migration
+  def change
+    create_table :addresses do |t|
+      t.string :street
+      t.string :zip_code
+
+      t.timestamps
+    end
+  end
+end
